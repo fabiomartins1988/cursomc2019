0688d77ac91531891b2ae418974b77201cb5e61c
Fabio Martins <fabiomsilva1388@gmail.com>
2019-03-04 14:15:15 -0300

    Ajustes no endpoint / categorias/{id}

--- src/main/java/com/fabiomartins/cursomc/domain/Produto.java
+++ src/main/java/com/fabiomartins/cursomc/domain/Produto.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Produto implements Serializable{
 	
@@ -25,6 +27,7 @@ public class Produto implements Serializable{
 	
 	//Possui relação de N pra N entre a tabela Categoria e Produto
 	//utiliza-se ManyToMany para fazer mapeamento entrer quais tabelas vai ter a relação
+	@JsonBackReference
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA",
 		joinColumns = @JoinColumn(name = "produto_id"),
