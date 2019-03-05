cc7066b8828643da611d84d74a4bb9ad216b6e3f
Fabio Martins <fabiomsilva1388@gmail.com>
2019-03-05 20:33:27 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/fabiomartins/cursomc/services/PedidoService.java
@@ -0,0 +1,27 @@
+package com.fabiomartins.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fabiomartins.cursomc.domain.Pedido;
+import com.fabiomartins.cursomc.repositories.PedidoRepository;
+
+import javassist.tools.rmi.ObjectNotFoundException;
+
+
+@Service
+public class PedidoService {
+	
+	@Autowired //instanciada automaticamente injecao de dependencia ou inversão de controle  
+	private PedidoRepository repo;
+	
+	
+	
+	public Pedido find(Integer id) throws ObjectNotFoundException {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
