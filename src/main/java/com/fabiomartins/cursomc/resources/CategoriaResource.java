2925f3622e332a4e7be06456dd2693b2da42681a
Fabio Martins <fabiomsilva1388@gmail.com>
2019-03-06 16:27:15 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/fabiomartins/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/fabiomartins/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.fabiomartins.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fabiomartins.cursomc.domain.Categoria;
 import com.fabiomartins.cursomc.services.CategoriaService;
@@ -24,5 +28,14 @@ public class CategoriaResource {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	//resposta HTTP
 	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);  //Cod Status HTTP 201 novo recurso inserido com  sucesso
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		//fornece como argumento da URI
+		
+		return ResponseEntity.created(uri).build(); //gera o codigo 201 http (created)
+	}
 }
